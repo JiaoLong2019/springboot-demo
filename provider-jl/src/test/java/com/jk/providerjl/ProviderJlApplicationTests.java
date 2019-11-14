21518668c627597d7eec9350836b23bd0b77a79e
JiaoLong2019 <542180727@qq.com>
2019-11-14 19:30:29 +0800

    第二次上传

--- provider-jl/src/test/java/com/jk/providerjl/ProviderJlApplicationTests.java
+++ provider-jl/src/test/java/com/jk/providerjl/ProviderJlApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jk;
+package com.jk.providerjl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ProviderJlApplicationTests {
 
     @Test
     void contextLoads() {
