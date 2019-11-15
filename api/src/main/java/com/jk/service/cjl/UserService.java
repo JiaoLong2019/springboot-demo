3b6b84693376aae97511ad568ea60ac14e1114fb
CaoJunLong <2686624372@qq.com>
2019-11-15 13:45:52 +0800

    第二次提交

---
+++ api/src/main/java/com/jk/service/cjl/UserService.java
@@ -0,0 +1,11 @@
+package com.jk.service.cjl;
+
+import com.jk.model.cjl.User;
+import com.jk.model.cjl.YongHu;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<YongHu> queryUser();
+}
